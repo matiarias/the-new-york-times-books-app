0a5cea9c42a11c774b4b1804303746d710ebc1ba
Matias Arias <matt.arias182@gmail.com>
2022-09-02 23:59:51 -0300

    añadiendo react router al componente home

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import Home from "./pages/home/Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
     <>
-      <Home />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 };
